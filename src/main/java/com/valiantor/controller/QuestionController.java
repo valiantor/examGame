d7e079c44d4270d4ec8c4eb2647bf9157989fa87
valiantkui <528227496@qq.com>
2020-03-22 10:48:59 +0800

    question: 新增rest接口

---
+++ src/main/java/com/valiantor/controller/QuestionController.java
@@ -0,0 +1,39 @@
+package com.valiantor.controller;
+
+import com.valiantor.entity.Question;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    /**
+     * 根据关卡号查询当前关卡下的所有题
+     * @param lNo 关卡号
+     * @return 所有问题集合
+     */
+    @RequestMapping("findAllQuestionByLNo")
+    public List<Question> findAllQuestionByLNo(@RequestParam("lNo") int lNo){
+
+
+        return null;
+    }
+
+    /**
+     * 查询指定关卡下的指定分页的题
+     * @param lNo 关卡号
+     * @param currPage 当前页
+     * @param numPerPage 每页显示数量
+     * @return
+     */
+    @RequestMapping("findQuestionPageByLNo")
+    public List<Question> findQuestionPageByLNo(@RequestParam("lNo") int lNo, int currPage,int numPerPage){
+
+
+        return null;
+    }
+}
